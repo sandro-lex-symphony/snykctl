1013fd7d443b01c7e8b2e7e17381640dda7c7608
Sandro Lex <sandro.lex@symphony.com>
2021-12-21 15:55:03 +0100

    add count Issues

--- internal/domain/issues.go
+++ internal/domain/issues.go
@@ -8,6 +8,7 @@ import (
 )
 
 const issuesPathPath = "/org/%s/project/%s/issue/%s/paths"
+const issueCountPath = "/reporting/counts/issues/latest?groupBy=project,severity"
 
 type ProjectIssuesResult struct {
 	Issues []*Issue
@@ -31,6 +32,28 @@ type IssuePathsResult struct {
 type IssuePath struct {
 	Name    string
 	Version string
+}
+
+type IssueCountResults struct {
+	Results []IssueCountResult
+}
+
+type IssueCountResult struct {
+	Day     string
+	Results []ProjectIssueCount
+}
+
+type ProjectIssueCount struct {
+	ProjectId string
+	Count     int
+	Severity  Severity
+}
+
+type Severity struct {
+	Critical int
+	High     int
+	Medium   int
+	Low      int
 }
 
 type IssueData struct {
@@ -115,4 +138,46 @@ func GetProjectIssuePaths(client tools.HttpClient, org_id string, prj_id string,
 	}
 
 	return &result, nil
+}
+
+func IssueCount(client tools.HttpClient, org_id string) (IssueCountResults, error) {
+	var result IssueCountResults
+	str := fmt.Sprintf("{\"filters\": {\"orgs\": [\"%s\"] } }", org_id)
+
+	var jsonStr = []byte(str)
+	resp := client.RequestPost(issueCountPath, jsonStr)
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return result, fmt.Errorf("Issue count failed: %s ", resp.Status)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
+
+func FormatIssueCountResults(r IssueCountResults, output string) string {
+	var out string
+	var count, c, h, m, l int
+
+	for _, results := range r.Results {
+		for _, projectResult := range results.Results {
+			count += projectResult.Count
+			c += projectResult.Severity.Critical
+			h += projectResult.Severity.High
+			m += projectResult.Severity.Medium
+			l += projectResult.Severity.Low
+		}
+	}
+	if output == "html" {
+		header := "<tabsle><tr><td>Total</td><td>Critical</td><td>High</td><td>Medium</td><td>Low</td></tr>"
+		body := fmt.Sprintf("<tr><td>%d</td><td>%d</td><td>%d</td><td>%d</td><td>%d</td></tr>", count, c, h, m, l)
+		footer := "</table>"
+		out = header + body + footer
+	} else {
+		out = fmt.Sprintf("COUNT: %d\nCRITICAL: %d\nHIGH: %d\nMEDIUM: %d\nLOW: %d\n", count, c, h, m, l)
+	}
+	return out
 }
